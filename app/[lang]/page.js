104335744e714514b61cc9a23eb7237d99bca809
rexcape <53886131+rexcape@users.noreply.github.com>
2023-09-26 22:10:47 +0800

    feat: add notion-plus for i18n

--- app/[lang]/page.js
+++ app/[lang]/page.js
@@ -1,11 +1,14 @@
 import { Button, Title } from '@mantine/core'
+import { getTranslator } from '@/lib/notion'
 
-export default function Home() {
+export default async function Home({ params }) {
+  const t = await getTranslator('index', params.lang)
   return (
     <main>
       <Title>Hello world!</Title>
       <Button>Get started</Button>
       <div className="text-lg font-bold">Tailwindcss</div>
+      <div className="text-lg font-bold">i18n test: {t('1')}</div>
     </main>
   )
 }
