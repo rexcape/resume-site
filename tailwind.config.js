deb43b40c0113d3ad9f9db4f349a970ae5e4f0cf
rexcape <53886131+rexcape@users.noreply.github.com>
2023-09-26 21:55:12 +0800

    chore: set tailwind config to mantineui's default

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   content: ['./app/**/*.{js,css}', './components/**/*.{js,css}'],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        xs: '0.625rem',
+        sm: '0.75rem',
+        md: '1rem',
+        lg: '1.25rem',
+        xl: '2rem',
+      },
+      fontSize: {
+        xs: '0.75rem',
+        sm: '0.875rem',
+        md: '1rem',
+        lg: '1.125rem',
+        xl: '1.25rem',
+      },
+      lineHeight: {
+        xs: '1.4',
+        sm: '1.45',
+        md: '1.55',
+        lg: '1.6',
+        xl: '1.65',
+      },
+    },
   },
   plugins: [require('daisyui')],
   corePlugins: {
