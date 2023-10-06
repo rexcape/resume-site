e3f225d0ac5338c2ebcb176924badae4d955761d
rexcape <53886131+rexcape@users.noreply.github.com>
2023-10-06 11:54:49 +0800

    fix: seperate i18n config

---
+++ i18n.js
@@ -0,0 +1,7 @@
+const i18nConfig = {
+  locales: ['zh', 'en', 'ja'],
+  defaultLocale: 'zh',
+  routingStrategy: 'dynamicSegment',
+}
+
+export default i18nConfig
--- middleware.js
+++ middleware.js
@@ -1,10 +1,5 @@
 import { i18nRouter } from 'next-i18n-router'
-
-const i18nConfig = {
-  locales: ['zh', 'en', 'ja'],
-  defaultLocale: 'zh',
-  routingStrategy: 'dynamicSegment',
-}
+import i18nConfig from './i18n'
 
 export function middleware(req) {
   return i18nRouter(req, i18nConfig)
